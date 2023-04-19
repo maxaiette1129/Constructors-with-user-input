365c67cf192c1d328b71665fe0e6a0506ff67dbd
maxai <maxai@MAX>
2023-04-19 10:44:48 +0800

    Add project files.

---
+++ ConsoleApp2/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+     class Product
+    {
+        public int stock;
+        public string name;
+        public int cost;
+        public string company;
+        public int price;
+
+        public Product(int stock, string name, int cost, string company, int price)
+        {
+            this.stock = stock;
+            this.name = name;
+            this.cost = cost;
+            this.company = company;
+            this.price = price;
+
+            Console.WriteLine("Product");
+            Console.WriteLine("Name:" + name + "\n Stock: " + stock + 
+                "\n Cost: " + cost + 
+                "\n Company: " + company + 
+                "\n Price: " + price);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isFailure = false;
+
+            if (isFailure == true)
+            {
+            Console.WriteLine("Keep trying!");
+            }
+            else
+            {
+            Console.WriteLine("Brighter Future ahead!");
+            }
+
+
+        }
+    }
+}
+
